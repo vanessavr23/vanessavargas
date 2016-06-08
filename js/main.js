a4cff8efd7901be076abce3eeb919ad7cc424a71
vanessavr23 <vanessavargasron@gmail.com>
2016-06-07 19:47:04 -0500

    portafolio

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function maquina(contenedor,texto,intervalo){
+   // Calculamos la longitud del texto
+   longitud = texto.length; 
+   // Obtenemos referencia del div donde se va a alojar el texto.
+   cnt = document.getElementById(contenedor);
+   var i=0;
+   // Creamos el timer
+   timer = setInterval(function(){
+      // Vamos añadiendo letra por letra y la _ al final.
+      cnt.innerHTML = cnt.innerHTML.substr(0,cnt.innerHTML.length-1) + texto.charAt(i) + "|";
+      // Si hemos llegado al final del texto..
+      if(i >= longitud){
+         // Salimos del Timer y quitamos la barra baja (_)
+         clearInterval(timer);
+         
+         cnt.innerHTML = cnt.innerHTML.substr(0,longitud);
+         return true;
+      } else {
+         // En caso contrario.. seguimos
+         i++;
+      }},intervalo);
+};
+
+var texto = "FRONT-END DEVELOPER";
+// 100 es el intervalo de minisegundos en el que se escribirá cada letra.
+maquina("maquinas",texto,200);
+
+
